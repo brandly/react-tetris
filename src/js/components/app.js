aff54f11c8ec63317ce9a36aa9f8dcf6a52f906f
brandly <brandly13@gmail.com>
2017-10-23 13:10:59 -0400

    refactor so Tetris takes children function

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -7,7 +7,31 @@ var app = React.createClass({
     return (
       <div>
         <h1>Tetris</h1>
-        <Tetris />
+        <Tetris>
+          {({
+            HeldPiece,
+            Scoreboard,
+            Gameboard,
+            PieceQueue
+          }) => {
+            return (
+              <div className="row">
+                <div className="col-md-2">
+                  <HeldPiece />
+                  <Scoreboard />
+                </div>
+
+                <div className="col-md-4">
+                  <Gameboard />
+                </div>
+
+                <div className="col-md-4">
+                  <PieceQueue />
+                </div>
+              </div>
+            )
+          }}
+        </Tetris>
       </div>
     )
   }
--- src/js/components/tetris.js
+++ src/js/components/tetris.js
@@ -9,43 +9,14 @@ var PieceQueue = require('./piece-queue');
 var AppConstants = require('../constants/app-constants');
 var states = AppConstants.states;
 
-function gameState () {
-  return {
-    gameState: GameStore.getCurrentState()
-  };
-}
-
 var Tetris = React.createClass({
-  // getInitialState: function () {
-  //   return gameState();
-  // },
-
-  // componentWillMount: function () {
-  //   GameStore.addChangeListener(this._onChange);
-  // },
-
-  // _onChange: function () {
-  //   this.setState(gameState());
-  // },
-
   render: function () {
-    return (
-      <div className="row">
-        <div className="col-md-2">
-          <HeldPiece />
-          <Scoreboard />
-        </div>
-
-        <div className="col-md-4">
-          <Gameboard />
-        </div>
-
-        <div className="col-md-4">
-          <PieceQueue />
-        </div>
-
-      </div>
-    )
+    return this.props.children({
+      HeldPiece,
+      Scoreboard,
+      Gameboard,
+      PieceQueue
+    });
   }
 });
 
