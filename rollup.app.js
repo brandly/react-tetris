77512e0d768e919029cb7bd103027bddd1dee9fa
brandly <brandly13@gmail.com>
2021-06-21 01:50:28 -0400

    fix build

--- rollup.app.js
+++ rollup.app.js
@@ -3,6 +3,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: 'app/main.js',
@@ -11,6 +12,7 @@ export default {
     format: 'iife'
   },
   plugins: [
+    typescript(),
     babel({
       exclude: 'node_modules/**'
     }),
