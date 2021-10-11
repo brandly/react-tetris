239c10b86670487937433c17399563dfbf0098d6
brandly <brandly13@gmail.com>
2021-10-11 00:41:31 -0400

    remove old constants

--- src/js/constants/app-constants.ts
+++ src/js/constants/app-constants.ts
@@ -1,27 +1,4 @@
 export default {
-  actions: {
-    HARD_DROP: 'HARD_DROP',
-    MOVE_DOWN: 'MOVE_DOWN',
-    MOVE_LEFT: 'MOVE_LEFT',
-    MOVE_RIGHT: 'MOVE_RIGHT',
-    FLIP_CLOCKWISE: 'FLIP_CLOCKWISE',
-    FLIP_COUNTERCLOCKWISE: 'FLIP_COUNTERCLOCKWISE',
-    PAUSE: 'PAUSE',
-    RESUME: 'RESUME',
-    HOLD: 'HOLD'
-  },
-
-  states: {
-    PAUSED: 'PAUSED',
-    PLAYING: 'PLAYING',
-    LOST: 'LOST'
-  },
-
-  events: {
-    LINE_CLEARED: 'LINE_CLEARED',
-    PLAYER_LOST: 'PLAYER_LOST'
-  },
-
   // dimensions in "cells"
   GAME_WIDTH: 10,
   GAME_HEIGHT: 20,
