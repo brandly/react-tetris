e08d325928fad2bed51618f9d0b59666c18d4bb9
brandly <brandly13@gmail.com>
2021-10-24 15:59:39 -0400

    extract test function

--- test/index.ts
+++ test/index.ts
@@ -3,13 +3,17 @@ import { update, getInitialGame, Action } from '../src/js/stores/game-store';
 
 const ticks: Action[] = ['TICK', 'TICK', 'TICK'];
 
-(() => {
+const test = (description: string, fn: () => void) => {
+  try {
+    fn();
+  } catch (e) {
+    console.error(description);
+    throw e;
+  }
+};
+
+test('Expected new piece to start at top', () => {
   let result = ticks.reduce(update, getInitialGame());
   result = update(result, 'HOLD');
-
-  assert.deepStrictEqual(
-    result.piece.position.y,
-    0,
-    'Expected new piece to start at top'
-  );
-})();
+  assert.deepStrictEqual(result.piece.position.y, 0);
+});
