d3de2a69b0e1db47800e1220066246b7b463e594
brandly <brandly13@gmail.com>
2021-10-24 16:05:36 -0400

    test some basic cases

--- test/index.ts
+++ test/index.ts
@@ -17,3 +17,24 @@ test('Expected new piece to start at top', () => {
   result = update(result, 'HOLD');
   assert.deepStrictEqual(result.piece.position.y, 0);
 });
+
+test('Expected the piece to move left', () => {
+  let result = ticks.reduce(update, getInitialGame());
+  const initX = result.piece.position.x;
+  result = update(result, 'MOVE_LEFT');
+  assert.deepStrictEqual(result.piece.position.x, initX - 1);
+});
+
+test('Expected the piece to move right', () => {
+  let result = ticks.reduce(update, getInitialGame());
+  const initX = result.piece.position.x;
+  result = update(result, 'MOVE_RIGHT');
+  assert.deepStrictEqual(result.piece.position.x, initX + 1);
+});
+
+test('Expected the piece to move down', () => {
+  let result = ticks.reduce(update, getInitialGame());
+  const initY = result.piece.position.y;
+  result = update(result, 'MOVE_DOWN');
+  assert.deepStrictEqual(result.piece.position.y, initY + 1);
+});
