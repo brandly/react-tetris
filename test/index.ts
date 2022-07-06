12d5240beded41a5fd27ede4ef143a90fdbc060d
brandly <brandly13@gmail.com>
2022-07-05 23:11:21 -0400

    add more tests

--- test/index.ts
+++ test/index.ts
@@ -13,28 +13,56 @@ const test = (description: string, fn: () => void) => {
 };
 
 test('Expected new piece to start at top', () => {
-  let result = ticks.reduce(update, init());
-  result = update(result, 'HOLD');
-  assert.deepStrictEqual(result.piece.position.y, 0);
+  let game = ticks.reduce(update, init());
+  game = update(game, 'HOLD');
+  assert.strictEqual(game.piece.position.y, 0);
 });
 
 test('Expected the piece to move left', () => {
-  let result = ticks.reduce(update, init());
-  const initX = result.piece.position.x;
-  result = update(result, 'MOVE_LEFT');
-  assert.deepStrictEqual(result.piece.position.x, initX - 1);
+  let game = ticks.reduce(update, init());
+  const initX = game.piece.position.x;
+  game = update(game, 'MOVE_LEFT');
+  assert.strictEqual(game.piece.position.x, initX - 1);
 });
 
 test('Expected the piece to move right', () => {
-  let result = ticks.reduce(update, init());
-  const initX = result.piece.position.x;
-  result = update(result, 'MOVE_RIGHT');
-  assert.deepStrictEqual(result.piece.position.x, initX + 1);
+  let game = ticks.reduce(update, init());
+  const initX = game.piece.position.x;
+  game = update(game, 'MOVE_RIGHT');
+  assert.strictEqual(game.piece.position.x, initX + 1);
 });
 
 test('Expected the piece to move down', () => {
-  let result = ticks.reduce(update, init());
-  const initY = result.piece.position.y;
-  result = update(result, 'MOVE_DOWN');
-  assert.deepStrictEqual(result.piece.position.y, initY + 1);
+  let game = ticks.reduce(update, init());
+  const initY = game.piece.position.y;
+  game = update(game, 'MOVE_DOWN');
+  assert.strictEqual(game.piece.position.y, initY + 1);
+});
+
+test('Expected TOGGLE_PAUSE to change state without touching matrix', () => {
+  let game = ticks.reduce(update, init());
+  const initMatrix = game.matrix;
+
+  game = update(game, 'TOGGLE_PAUSE');
+  assert.strictEqual(game.matrix, initMatrix);
+  assert.strictEqual(game.state, 'PAUSED');
+
+  game = update(game, 'TOGGLE_PAUSE');
+  assert.strictEqual(game.matrix, initMatrix);
+  assert.strictEqual(game.state, 'PLAYING');
+});
+
+test('Expected HOLD to hold piece just once', () => {
+  let game = ticks.reduce(update, init());
+  const initPiece = game.piece.piece;
+  assert.strictEqual(game.heldPiece, undefined);
+
+  game = update(game, 'HOLD');
+  assert.strictEqual(game.heldPiece.piece, initPiece);
+  assert.strictEqual(game.heldPiece.available, false);
+
+  // Holding a second time has no effect!
+  const beforeGame = game;
+  game = update(game, 'HOLD');
+  assert.strictEqual(beforeGame, game);
 });
